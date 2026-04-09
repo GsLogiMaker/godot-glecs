fa7544de13e718bddc59b1d2bf1c4264f013dfb7
GsLogimaker <gslogimaker@gmail.com>
2026-04-09 07:27:30 -0500

    Fix entities registering in wrong module

--- gd/rendering/_module.gd
+++ gd/rendering/_module.gd
@@ -1,12 +1,11 @@
 extends GFModule
 
-
 const Systems:= preload("./_systems.gd")
 
 func _register(world: GFWorld) -> void:
 	set_name("rendering")
 	world.register_script(GFCanvasItem)
+	world.register_script(GFColor)
 	world.register_script(GFRect2D)
 	world.register_script(GFOnDraw)
 	world.register_script(GFTexture2D)
-	world.register_script(Systems)
--- gd/rendering/rect2d.gd
+++ gd/rendering/rect2d.gd
@@ -3,6 +3,8 @@ class_name GFRect2D extends GFTag
 
 func _register(world:GFWorld) -> void:
 	add_pair("/root/flecs/core/With", GFCanvasItem)
+	world.register_script(color).set_name("Color")
+	world.register_script(size).set_name("Size")
 
 class color extends GFColor: pass
 class size extends GFSize2D: pass
